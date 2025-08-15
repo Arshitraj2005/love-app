6fef430e3c7061ff6ee3d8ed0522f379eae29552
Arshitraj2005 <nigachu789@gmail.com>
2025-08-15 12:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from threading import Lock
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+messages = {}
+message_order = []
+lock = Lock()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @socketio.on('send_message')
-def handle_message(data):
+def handle_send_message(data):
     with lock:
-        msg_id = str(len(messages) + 1)
-        messages[msg_id] = {
-            'message': data['message'],
-            'sender_id': data['sender_id']
-        }
-        message_order.append(msg_id)
+        sender = data['sender']
+        message = data['message']
+        messages[sender] = message
+        message_order.append(sender)
 
-        emit('new_message', {
-            'id': msg_id,
-            'message': data['message'],
-            'sender_id': data['sender_id']
-        }, broadcast=True)
-
-        # Keep only latest 8 messages
         if len(message_order) > 8:
-            oldest_id = message_order.pop(0)
-            messages.pop(oldest_id, None)
-            emit('delete_message', {'id': oldest_id}, broadcast=True)
+            oldest = message_order.pop(0)
+            messages.pop(oldest, None)
+
+        emit('receive_message', {'messages': messages}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=10000)
