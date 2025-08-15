382477ea874ac4cd9f7c82a8b7e24bf70bb1ec35
Arshitraj2005 <nigachu789@gmail.com>
2025-08-15 13:02:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from collections import defaultdict
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-messages = {}
+messages = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('send_message')
-def handle_send_message(data):
-    sender = data['sender_id']
-    message = data['message']
-    messages[sender] = message
-    emit('receive_message', {'messages': messages}, broadcast=True)
+def handle_send(data):
+    messages.append(data)
+    emit('receive_message', data, broadcast=True)
+
+@socketio.on('typing')
+def handle_typing(data):
+    emit('typing', data, broadcast=True)
+
+@socketio.on('seen')
+def handle_seen(data):
+    emit('seen', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=10000)
+    socketio.run(app, debug=True)
